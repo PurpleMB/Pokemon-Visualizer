dfc9b725a4e6e996cf34a77bf1d40fc45be0f75d
PurpleMB <purplemb816@gmail.com>
2024-09-02 21:59:13 -0500

    Bug Fix: Img loading issues
    *Fixed issue with loading images
    *Moved non-image info to separate file

--- Assets/Scripts/Visualization/Visualizer.cs
+++ Assets/Scripts/Visualization/Visualizer.cs
@@ -15,15 +15,22 @@ public class Visualizer : MonoBehaviour
 
     public bool SetVisualizedPokemon(Pokemon pokemon)
     {
+        Debug.Log("wtf is going on");
         Pokemon oldPoke = _visualizedPoke;
         _visualizedPoke = pokemon;
 
-        string imgPath = $"Images/{_visualizedPoke.id.ToString("000")}.png";
+        string imgPath = $"{_visualizedPoke.id.ToString("000")}";
         Debug.Log("Visualizer attempting to load image at path: " + imgPath);
+        /*
         Texture2D pokeTex = Resources.Load<Texture2D>(imgPath);
         Debug.Log(pokeTex == null);
         Rect rec = new Rect(0, 0, pokeTex.width, pokeTex.height);
         _pokeImg.sprite = Sprite.Create(pokeTex, rec, new Vector2(0, 0), 1);
+        */
+
+        Sprite pokeSprite = Resources.Load<Sprite>(imgPath);
+        Debug.Log($"Image Loaded Successfully: {pokeSprite != null}");
+        _pokeImg.sprite = pokeSprite;
 
         _pokeText.text = _visualizedPoke.name.english;
 
